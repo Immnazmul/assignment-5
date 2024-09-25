da71fb1cba14c912fc0e87e4b06e38f6da797b97
Immnazmul <islam.mdnazmul1@gmail.com>
2024-09-25 09:34:44 +0600

    added history function

--- js/balance.js
+++ js/balance.js
@@ -11,6 +11,54 @@ function getTextFieldValueById(id){
     const textNumber = parseFloat(textValue);
     return textNumber;
 }
+document.getElementById('btn-donate-now')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoney = getInputFieldValueById('input-add-money');
+        
+
+        if(isNaN(addMoney)){
+            alert('Failed to donate. Invalid amount');
+            return;
+        }
+      
+        if(addMoney > 0){
+        
+            const balance = getTextFieldValueById('account-balance');
+            const newBalance = balance + addMoney;
+
+            document.getElementById('account-balance').innerText = newBalance;
+
+            // add to transaction history
+            const div = document.createElement('div');
+            div.style.border='thin solid #C7C6C5';
+            div.style.fontWeight= 'bold' ;
+            div.style.textAlign= 'center';
+            div.innerHTML = `
+                <p class="text-2xl">${addMoney} Taka Is Donated For Flood In Noakhali, Bangladesh</p>
+            `
+
+            document.getElementById('transaction-container').appendChild(div);
+        
+          
+            const p = document.createElement('p');
+            p.innerText = Date();
+            console.log(p); 
+            div.appendChild(p);
+        }
+        else{
+            alert('Failed to donate. Invalid amount.')
+        }
+
+    })
+
+
+
+
+
+
+
 
 function showSectionById(id){
     // hide all the sections
