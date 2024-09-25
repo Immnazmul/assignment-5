ae219eb0870dbf0813e25137a4918a177256b078
Immnazmul <islam.mdnazmul1@gmail.com>
2024-09-25 07:08:52 +0600

    added hidden function

--- js/balance.js
+++ js/balance.js
@@ -1,1 +1,33 @@
-console.log(check);
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    
+    return inputNumber;
+}
+
+
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber;
+}
+
+function showSectionById(id){
+    // hide all the sections
+    document.getElementById('donation-section').classList.add('hidden');
+    document.getElementById('transaction-section').classList.add('hidden');
+    
+    // show the section with the provide id as parameter
+    document.getElementById(id).classList.remove('hidden');
+}
+document.getElementById('show-donation-section')
+    .addEventListener('click', function () {
+        showSectionById('donation-section');
+    });
+
+    document.getElementById('show-transaction-history')
+    .addEventListener('click', function () {
+        showSectionById('transaction-section');
+
+    });
+
